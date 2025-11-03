c7a01095d262365025c1049cf4e6adf6dade078e
ChoSeongHyeon <00seong116@naver.com>
2025-11-03 17:10:35 +0900

    feat(model): Rank enum에 등수 및 판별로직 구현

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,38 @@
+package lotto.model;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean bonus;
+    private final int prize;
+
+    Rank(int matchCount, boolean bonus, int prize) {
+        this.matchCount = matchCount;
+        this.bonus = bonus;
+        this.prize = prize;
+    }
+
+    public static Rank valueOf(int matchCount, boolean bonus) {
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .filter(rank -> rank.bonus == bonus)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
